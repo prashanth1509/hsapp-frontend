3848a8157718f8925f9a6a37a1a53d3130720b0f
prashanth.a <prashanth.a@flipkart.com>
2015-04-25 20:02:54 +0530

    update-gulp-build

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,188 @@
 /*
-	Gulp related deps
-*/
+ Gulp app builder.
+ Usage: Refer https://github.com/prashanth1509/richview-gulp
+ 18/03/2015
+ */
+
+/*
+ Dependencies
+ */
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
+	sass = require('gulp-ruby-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	minifycss = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	gulp_webpack = require('gulp-webpack'),
 	$ = require('gulp-load-plugins')();
 
-//CHANGE NAME OF APP
-var APP = "dev";
+var webpack = require('webpack'),
+	_ = require('lodash');
 
-var dev_dir = {
-	css: 'src/css/' + APP,
-	js: 'src/js/' + APP
+/*
+ Overridable default configs through commandline
+ */
+var config = {
+	appname: 'all',
+	appdir: 'apps/**',
+	cssdir: 'apps/', //Why 'apps/' instead of 'apps/**'? -> We're feeding this to sass() and not gulp.src()
+	jsdir: 'apps/**/*.js',
+	skeleton: 'generators/app-skeleton'
 };
 
-var local_build_dir_dest = '/Users/prashanth.a/me/php-apps/drisle-static/';
+/*
+ Output directories
+ */
 var build_dir = {
-	//css: 'build/css/' + APP,
-	//js: 'build/js/' + APP
-    css: local_build_dir_dest + 'css/' + APP,
-    js: local_build_dir_dest + 'js/' + APP
-};
+	css: 'build/css',
+	js: 'build/js'
+}
 
+//Will hold commandline options
 var options = {};
 
+gulp.task('default', function(){
+	var args = process.argv;
+
+	//parse commandline arguments
+	_.each(args,function(value, index){
+		if(value.substr(0,2)=='--'){
+			var mixedKeyValue = value.replace('--', '').split('=');
+			options[mixedKeyValue[0]]=mixedKeyValue[1];
+		}
+	});
+
+	//prepare configurations
+	gulp.start('_configure');
+});
+
+gulp.task('_configure', function(){
+	if(options.appdir && options.appdir.trim().length){
+		var appdir = options.appdir;
+		options.appname = options.appname?options.appname:appdir;
+		options.cssdir = 'apps/' + appdir + '/';
+		options.jsdir = 'apps/' + appdir + '/components/**/';
+		options.appdir = 'apps/' + appdir + '/**';
+	}
+	_.assign(config, options);
+	//console.log('\nUSING CONFIGURATION: ', JSON.stringify(config), '\n');
+
+	//start watching for changes within app
+	gulp.start('watch');
+});
+
+/*
+ Builds scss into css and puts into build/css
+ */
 gulp.task('css-build', function() {
 
-	return sass( dev_dir.css, { compass: true, style: 'expanded' })
-        //.pipe(concat('build.min.css'))
-        .pipe(gulp.dest(build_dir.css));
+	console.log('\nCOMPILING CSS: gulp.src/sass=', config.cssdir, ' gulp.dest=', config.appname);
+
+	//to generate file with version code
+	var versionedFilename = config.appname + '-v' + (new Date().getDate()) + '.css';
+	//to generate same file with prefix -lastest
+	var latestFilename = config.appname + '-latest.css';
+
+
+	return sass( config.cssdir, { style: 'expanded' })
+		//.pipe(minify()) //uncomment and initialize at top, for minification.
+		//concat all files
+		.pipe(concat(versionedFilename))
+		//save it
+		.pipe(gulp.dest(build_dir.css))
+		//rename it
+		.pipe($.rename(latestFilename))
+		//save it again!
+		.pipe(gulp.dest(build_dir.css));
 
 });
 
 /*
-	Builds js files with src/js to build/js
-*/
+ Builds js files with src/js to build/js
+ */
 gulp.task('js-build', function(){
 
-	return gulp.src( dev_dir.js + '/**/*.js' )
-        //.pipe(concat('build.min.js'))
-        .pipe(gulp.dest(build_dir.js));
+	console.log('\nCOMPILING JS: gulp.src=', config.jsdir, ' gulp.dest=', config.appname);
+
+	var versionedFilename = config.appname + '-v' + (new Date().getDate()) + '.js';
+	var latestFilename = config.appname + '-latest.js';
+
+	var package_config = require('./package.json')
+	//Realize the shared components in package.json
+	sharedDependencies = package_config.sharedDependencies?package_config.sharedDependencies:[];
+
+	/*
+	 return gulp.src( config.jsdir )
+	 //.pipe(minify())
+	 .pipe(concat(versionedFilename))
+	 .pipe(gulp.dest('build/js/'))
+	 .pipe($.rename(latestFilename))
+	 .pipe(gulp.dest('build/js/'));
+	 */
+
+	return gulp_webpack(
+		{
+			entry: {
+				//look into entry point
+				app: './apps/' + config.appname + '/entry.js',
+				//save shared dependencies as separate file
+				shared: sharedDependencies,
+			},
+			output: {
+				filename: versionedFilename
+			},
+			plugins: [
+				new webpack.optimize.CommonsChunkPlugin('shared', 'shared.bundle.js')
+			]
+		}
+	)
+		.pipe(gulp.dest(build_dir.js))
+		//save another copy with -latest prefix
+		.pipe($.rename(latestFilename))
+		.pipe(gulp.dest(build_dir.js));
 
 });
 
 /*
-	Watches for changes in file
-*/
-gulp.task('default', function() {
+ Watches for changes in file
+ */
+gulp.task('watch', function() {
 
-	gulp.start(['js-build','css-build']);	
-	return gulp.watch('src/**/**', ['js-build', 'css-build']);
+	var watchPath = config.appdir;
 
-});
+	var fs = require('fs');
+	var dir = 'apps/' + config.appname;
 
+	//if app doesn't exist create one.
+	//--todo ask user for confirmation
+	if (!fs.existsSync(dir)){
+		fs.mkdirSync(dir);
+		//create app skeleton within app directory.
+		var ncp = require('ncp').ncp;
+		ncp.limit = 16;
+		ncp(config.skeleton, dir, function (err) {
+			if(err)
+				console.log('Cannot create skeleton directory please create one and try again.');
+			else{
+				console.log('App bare-bone generated successfully. See: ' + dir);
+				//Start watching for css/js changes
+				startWatch();
+			}
+		});
 
-gulp.task('cloud-deploy', function(){
+	}
+	else{
+		//Start watching for css/js changes
+		startWatch();
+	}
 
-    var bitballoon = require("bitballoon");
-    //console.log(process.env.ACCESSKEY, process.env.SITEID);return;
-    console.log("Starting deployment to cloud..");
-    bitballoon.deploy({access_token: process.env.ACCESSKEY, site_id: process.env.SITEID, dir: local_build_dir_dest}, function(err, deploy) {
-        if (err) { return console.log(err); }
-        console.log("New deploy is live");
-    });
+	function startWatch(){
+
+		console.log('\nWATCHING: ', watchPath, '\n');
+
+		return gulp.watch(watchPath, ['js-build', 'css-build']);
+		return gulp.start(['js-build','css-build']);
+	}
+
 
 });
